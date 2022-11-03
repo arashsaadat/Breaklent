fd4c307dd2ac22dc0478d74e9eac97cc7841ea2e
arashsaadat <98591525+arashsaadat@users.noreply.github.com>
2022-11-03 22:37:41 +0330

    new

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
+	$(".cart-number-up").click(function () {
+		var value = parseInt($(this).parent().find("input[type=number]").val());
+		if (value < 5) {
+			$(this)
+				.parent()
+				.find("input[type=number]")
+				.val(value + 1);
+		}
+	});
 
-    $(".cart-number-up").click(function(){
-        var value = parseInt($(this).parent().find('input[type=number]').val());
-        if(value < 5) {
-            $(this).parent().find('input[type=number]').val(value + 1);
-        }
-    });
-
-    $(".cart-number-down").click(function(){
-        var value = parseInt($(this).parent().find('input[type=number]').val());
-        if(value > 1) {
-            $(this).parent().find('input[type=number]').val(value - 1);
-        }
-    });
-
+	$(".cart-number-down").click(function () {
+		var value = parseInt($(this).parent().find("input[type=number]").val());
+		if (value > 1) {
+			$(this)
+				.parent()
+				.find("input[type=number]")
+				.val(value - 1);
+		}
+	});
+	$(".addres-item").click(function () {
+		$(".addres-item").removeClass("active");
+		$(".active-address-section").removeClass("active");
+		$(this).addClass("active");
+		$(this).find(".active-address-section").addClass("active");
+	});
+    $(".select-post-item").click(function () {
+		$(".select-post-item").removeClass("active");
+		$(this).addClass("active");
+	});
 });
+
